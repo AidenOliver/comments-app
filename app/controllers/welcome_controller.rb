eca89387580641c7d19c77c93c954090b4e200a8
Aiden Oliver <aiden.oliver99@gmail.com>
2020-03-22 16:56:40 -0600

    Added feature to automatically delete oldest comments once comments reached the comment limit which is 10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @comments = Comment.all
+
+    if @comments.length >= 10
+      last_comment = Comment.first
+      last_comment.destroy
+    end
   end
 end
